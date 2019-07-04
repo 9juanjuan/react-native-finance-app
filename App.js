5e7336f5f63c79f987f8ed4a28ef3059441784ad
Antonio Garcia <antonio.jgarcia100@gmail.com>
2019-07-04 17:01:59 -0500

    wraps App js in SwitchNavigator

--- App.js
+++ App.js
@@ -40,155 +40,150 @@ today = yyyy + '-' + mm + '-' + dd
 
 
 export default class App extends React.Component {
-  state = {
-        type: 'income',
-        amount: '',
-        category: '',
-        description: '',
-        color: '',
-        chosenDate: today,
-        submitted: false
-      }
-  
-  _getUserInput = () => {
-    console.log(parseInt(this.state.amount));
-    if(isNaN(parseInt(this.state.amount)) || this.state.description=== '' ){
-      console.log('No description or not a number')
-    } else {
-    if (this.state.type === 'income') {
-      let randColor = colorChooser.greens[Math.floor(Math.random()*7)];
-      data.push({
-        name: this.state.description+' +'+parseInt(this.state.amount),
-        amount: parseInt(this.state.amount),
-        color: randColor,   
-        legendFontColor: 'green',
-        legendFontSize: 10,
-        date: this.state.chosenDate,
-        type: this.state.type,
-        id: uuid()
-      });
-      
-      // data.map((entry)=> {
-      //   console.log(entry.type)
-      //   if (entry.type === 'income' && entry.id in data === false) {
-          incomeData.push([this.state.chosenDate, this.state.description, this.state.amount])
-      //   }
-      // })
-      console.log(incomeData);
-
-    } else if (this.state.type ==='expense') {
-      let randColor = colorChooser.reds[Math.floor(Math.random()*7)];
-      data.push({
-        name: this.state.description+ ' -'+parseInt(this.state.amount),
-        amount: parseInt(this.state.amount),
-        color: randColor,   
-        legendFontColor: 'red',
-        legendFontSize: 10,
-        date: this.state.chosenDate,
-        type: this.state.type,
-        id: uuid()
-      });
-      // data.map((entry)=> {
-      //   if (entry.type === 'expense' ) {
-      //     expenseData.push([entry.date, this.state.description, entry.amount])
-      //   }
-      // })
-      expenseData.push([this.state.chosenDate, this.state.description, this.state.amount])
-    }
-  }
-
-  
-    // Resets input fields
-    this.setState({
-      type: 'income',
-      amount: '',
-      category: '',
-      description: '',
-      submitted: true
-    })
-  }
-  _getUserText = (text) => {
-    console.log(text);
-    this.setState({
-        description: text 
-    })
-  }
-  _getUserAmount = (text) => {
-    console.log(text);
-    this.setState({
-        amount: text
-    })
-    console.log(this.state.amount)
-  }
-  _getType = (itemValue, itemIndex ) => {
-    const value = itemValue 
-    this.setState({
-        type: value
-    })
-  }
-  _setDate = (newDate) => {
-    console.log(newDate)
-    this.setState({chosenDate: newDate});
-    
-  }
-
-
-  render () {
-    return (
-      //  AppContainer will contain the app eventually but need to figure out how to wrap existing app components
-    
-      // <AppContainer/>
-        <View style={styles.container}>
-          <Title/>
-          <InputForm formHandler={this._getUserInput} descriptionHandler={this._getUserText} typeHandler={this._getType} amountHandler={this._getUserAmount} dateHandler={this._setDate} description={this.state.description} type={this.state.type} amount={this.state.amount} date={this.state.chosenDate} />
-        {/* </View> */}
-        { this.state.submitted ?
-             <ScrollView
-             style={{borderWidth: 1, height: 300}}
-             horizontal={true}
-             >
-           <View style={styles.chart}>
-              <Text>Percentage Breakdown</Text>
-                <PieChart
-                    data={data}
-                    width={screenWidth}
-                    height={200}
-                    chartConfig={{
-                      backgroundGradientFrom: '#1E2923',
-                      backgroundGradientTo: '#08130D',
-                      color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`
-                    }}
-                    accessor="amount"
-                    backgroundColor="transparent"
-                  />
-               <Text>Swipe for chart</Text>
-             </View>
-             <IncomeTable data={incomeData}/>
-             <ExpenseTable data={expenseData}/>
-             <LineChart 
-                data ={lineData}
-                height={200}
-                width={screenWidth}
-                chartConfig={chartConfig}
-
-              />
-             </ScrollView>
-          
-          : null}
-       
-      </View>
-    );
+    render() {
+      return <AppContainer/>
   }
 }
 
 class DashboardScreen extends Component {
-  render () {
-    return (
-      <View style={{ flex:1, alignItems: 'center', justifyContent: 'center'}}>
-        <Text> DashboardScreen</Text>
-      </View>
-    )
+  state = {
+    type: 'income',
+    amount: '',
+    category: '',
+    description: '',
+    color: '',
+    chosenDate: today,
+    submitted: false
   }
+
+_getUserInput = () => {
+console.log(parseInt(this.state.amount));
+if(isNaN(parseInt(this.state.amount)) || this.state.description=== '' ){
+  console.log('No description or not a number')
+} else {
+if (this.state.type === 'income') {
+  let randColor = colorChooser.greens[Math.floor(Math.random()*7)];
+  data.push({
+    name: this.state.description+' +'+parseInt(this.state.amount),
+    amount: parseInt(this.state.amount),
+    color: randColor,   
+    legendFontColor: 'green',
+    legendFontSize: 10,
+    date: this.state.chosenDate,
+    type: this.state.type,
+    id: uuid()
+  });
+  
+  // data.map((entry)=> {
+  //   console.log(entry.type)
+  //   if (entry.type === 'income' && entry.id in data === false) {
+      incomeData.push([this.state.chosenDate, this.state.description, this.state.amount])
+  //   }
+  // })
+  console.log(incomeData);
+
+} else if (this.state.type ==='expense') {
+  let randColor = colorChooser.reds[Math.floor(Math.random()*7)];
+  data.push({
+    name: this.state.description+ ' -'+parseInt(this.state.amount),
+    amount: parseInt(this.state.amount),
+    color: randColor,   
+    legendFontColor: 'red',
+    legendFontSize: 10,
+    date: this.state.chosenDate,
+    type: this.state.type,
+    id: uuid()
+  });
+  // data.map((entry)=> {
+  //   if (entry.type === 'expense' ) {
+  //     expenseData.push([entry.date, this.state.description, entry.amount])
+  //   }
+  // })
+  expenseData.push([this.state.chosenDate, this.state.description, this.state.amount])
+}
+}
+
+
+// Resets input fields
+this.setState({
+  type: 'income',
+  amount: '',
+  category: '',
+  description: '',
+  submitted: true
+})
+}
+_getUserText = (text) => {
+console.log(text);
+this.setState({
+    description: text 
+})
+}
+_getUserAmount = (text) => {
+console.log(text);
+this.setState({
+    amount: text
+})
+console.log(this.state.amount)
+}
+_getType = (itemValue, itemIndex ) => {
+const value = itemValue 
+this.setState({
+    type: value
+})
+}
+_setDate = (newDate) => {
+console.log(newDate)
+this.setState({chosenDate: newDate});
+
+}
+
+
+render () {
+return (
+  //  AppContainer will contain the app eventually but need to figure out how to wrap existing app components
+
+  // <AppContainer/>
+    <View style={styles.container}>
+      <Title/>
+      <InputForm formHandler={this._getUserInput} descriptionHandler={this._getUserText} typeHandler={this._getType} amountHandler={this._getUserAmount} dateHandler={this._setDate} description={this.state.description} type={this.state.type} amount={this.state.amount} date={this.state.chosenDate} />
+    {/* </View> */}
+    { this.state.submitted ?
+         <ScrollView
+         style={{borderWidth: 1, height: 300}}
+         horizontal={true}
+         >
+       <View style={styles.chart}>
+          <Text>Percentage Breakdown</Text>
+            <PieChart
+                data={data}
+                width={screenWidth}
+                height={200}
+                chartConfig={{
+                  backgroundGradientFrom: '#1E2923',
+                  backgroundGradientTo: '#08130D',
+                  color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`
+                }}
+                accessor="amount"
+                backgroundColor="transparent"
+              />
+           <Text>Swipe for chart</Text>
+         </View>
+         <IncomeTable data={incomeData}/>
+         <ExpenseTable data={expenseData}/>
+         <LineChart 
+            data ={lineData}
+            height={200}
+            width={screenWidth}
+            chartConfig={chartConfig}
+
+          />
+         </ScrollView>
+      
+      : null}
+   
+  </View>
+  )};
 }
 
 class SpreadsheetScreen extends Component {
